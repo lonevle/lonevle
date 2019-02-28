4a4e631f6d0b168fe827a585894613678f5823e6
lonevle <48077551+lonevle@users.noreply.github.com>
2019-02-28 12:30:06 +0800

    Add files via upload

---
+++ kx/static/js/config.js
@@ -0,0 +1,3 @@
+var ios = "itms-services://?action=download-manifest&url=https://www.kxcmchess.com/kxcm_2.0.2.plist";
+var az = "http://down.kxcm.91wqp.com/APK/kxcm_2.0.2.apk";
+var main = "http://www.kxcmchess.com";
