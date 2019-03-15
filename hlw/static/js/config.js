8842f16871396953fb0ec92cd681b0e858b72c6f
lonevle <48077551+lonevle@users.noreply.github.com>
2019-03-16 02:51:07 +0800

    Add files via upload

---
+++ hlw/static/js/config.js
@@ -0,0 +1,3 @@
+var ios = "itms-services://?action=download-manifest&url=https://www.hollywcs.com/hlw.plist";
+var az = "http://down.hlw.91wqp.com/APK/%E5%A5%BD%E8%8E%B1%E5%9D%9E1.0.2.apk";
+var main = "https://www.hollywcs.com";
